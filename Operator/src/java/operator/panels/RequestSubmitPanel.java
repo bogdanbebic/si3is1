33e52d99ba82c7f8b891b676abcc0bbe9aa4431e
Bogdan Bebić <bb170011d@student.etf.bg.ac.rs>
2020-02-05 15:32:45 +0100

    Implemented submit button

--- Operator/src/java/operator/panels/RequestSubmitPanel.java
+++ Operator/src/java/operator/panels/RequestSubmitPanel.java
@@ -13,8 +13,8 @@ import operator.Main;
  * @author Bogdan
  */
 public class RequestSubmitPanel extends JPanel {
-    private JLabel label = new JLabel("Request submit");
     private JButton backButton = new JButton("Back to menu");
+    private JLabel label = new JLabel("Request submit");
     
     {
         this.backButton.addActionListener(e -> {
@@ -22,8 +22,22 @@ public class RequestSubmitPanel extends JPanel {
         });
     }
     
+    private JButton submitButton = new JButton("Submit");
+
     {
-        this.add(this.backButton);
-        this.add(this.label);
+        this.submitButton.addActionListener(e -> {
+            // TODO: implement
+            System.out.println("SUBMIT");
+        });
+    }
+    {
+        this.backButton.setAlignmentX(JComponent.CENTER_ALIGNMENT);
+        this.label.setAlignmentX(JComponent.CENTER_ALIGNMENT);
+        this.submitButton.setAlignmentX(JComponent.CENTER_ALIGNMENT);
+    }
+    
+    {
+        this.add(this.submitButton);
+        this.add(Box.createVerticalGlue());
     }
 }
