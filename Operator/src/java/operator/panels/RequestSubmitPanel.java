f47dbdb7767daf6448b7804777ed00f82334dd05
Bogdan Bebić <bb170011d@student.etf.bg.ac.rs>
2020-02-05 18:03:43 +0100

    Improved GUI elements of RequestsInfoPanel and RequestSubmitPanel

--- Operator/src/java/operator/panels/RequestSubmitPanel.java
+++ Operator/src/java/operator/panels/RequestSubmitPanel.java
@@ -5,6 +5,8 @@
  */
 package operator.panels;
 
+import java.awt.Color;
+import java.awt.Graphics;
 import javax.swing.*;
 import operator.Main;
 
@@ -13,8 +15,8 @@ import operator.Main;
  * @author Bogdan
  */
 public class RequestSubmitPanel extends JPanel {
-    private JButton backButton = new JButton("Back to menu");
-    private JLabel label = new JLabel("Request submit");
+    private final JButton backButton = new JButton("Back to menu");
+    private final JLabel label = new JLabel("Request submit");
     
     {
         this.backButton.addActionListener(e -> {
@@ -22,7 +24,7 @@ public class RequestSubmitPanel extends JPanel {
         });
     }
     
-    private JButton submitButton = new JButton("Submit");
+    private final JButton submitButton = new JButton("Submit");
 
     {
         this.submitButton.addActionListener(e -> {
@@ -37,9 +39,23 @@ public class RequestSubmitPanel extends JPanel {
     }
     
     {
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.add(Box.createVerticalStrut(10));
         this.add(this.backButton);
+        this.add(Box.createVerticalStrut(10));
         this.add(this.label);
+        this.add(Box.createVerticalStrut(10));
+        this.add(new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.setColor(Color.blue);
+                g.fillRect(0, 0, this.getWidth() - 1, this.getHeight() - 1);
+            }
+            
+        });
+        this.add(Box.createVerticalStrut(10));
         this.add(this.submitButton);
-        this.add(Box.createVerticalGlue());
+        this.add(Box.createVerticalStrut(10));
     }
 }
--- Operator/src/java/operator/panels/RequestsInfoPanel.java
+++ Operator/src/java/operator/panels/RequestsInfoPanel.java
@@ -5,6 +5,8 @@
  */
 package operator.panels;
 
+import java.awt.Color;
+import java.awt.Graphics;
 import javax.swing.*;
 import operator.Main;
 
@@ -13,8 +15,8 @@ import operator.Main;
  * @author Bogdan
  */
 public class RequestsInfoPanel extends JPanel {
-    private JButton backButton = new JButton("Back to menu");
-    private JLabel label = new JLabel("Requests info");
+    private final JButton backButton = new JButton("Back to menu");
+    private final JLabel label = new JLabel("Requests info");
     
     {
         this.backButton.addActionListener(e -> {
@@ -28,7 +30,21 @@ public class RequestsInfoPanel extends JPanel {
     }
     
     {
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.add(Box.createVerticalStrut(10));
         this.add(this.backButton);
+        this.add(Box.createVerticalStrut(10));
         this.add(this.label);
+        this.add(Box.createVerticalStrut(10));
+        this.add(new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                g.setColor(Color.green);
+                g.fillRect(0, 0, this.getWidth() - 1, this.getHeight() - 1);
+            }
+            
+        });
+        this.add(Box.createVerticalStrut(10));
     }
 }
