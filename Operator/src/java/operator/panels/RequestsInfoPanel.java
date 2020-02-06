49380af8fe2435e6be4eeb0c00a157ff5f390ddf
Bogdan Bebić <bb170011d@student.etf.bg.ac.rs>
2020-02-06 22:51:43 +0100

    Implemented GUI for RequestsInfoPanel

--- Operator/src/java/operator/panels/RequestsInfoPanel.java
+++ Operator/src/java/operator/panels/RequestsInfoPanel.java
@@ -5,8 +5,6 @@
  */
 package operator.panels;
 
-import java.awt.Color;
-import java.awt.Graphics;
 import javax.swing.*;
 import operator.Main;
 
@@ -16,7 +14,7 @@ import operator.Main;
  */
 public class RequestsInfoPanel extends JPanel {
     private final JButton backButton = new JButton("Back to menu");
-    private final JLabel label = new JLabel("Requests info");
+    private final JLabel label = new JLabel("Requests info:");
     
     {
         this.backButton.addActionListener(e -> {
@@ -24,11 +22,42 @@ public class RequestsInfoPanel extends JPanel {
         });
     }
     
+    private JLabel idLabel = new JLabel("ID of request (zero padded 7 digits):");
+    private JTextField idTextField = new JTextField();
+    
+    private JButton refreshButton = new JButton("Refresh");
+    {
+        this.refreshButton.addActionListener(e -> {
+            String idRequest = this.idTextField.getText();
+            if (idRequest.length() != 7) {
+                JOptionPane.showMessageDialog(this, "Length must be 7", "Error", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+            
+            try {
+                Integer.parseUnsignedInt(idRequest);
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(this, "ID must be integer", "Error", JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+            
+            // TODO: send refresh to server
+            
+            System.out.println("REFRESH");
+        });
+    }
+    
+    // sets component alignment
     {
         this.backButton.setAlignmentX(JComponent.CENTER_ALIGNMENT);
         this.label.setAlignmentX(JComponent.CENTER_ALIGNMENT);
+        
+        this.idLabel.setAlignmentX(JComponent.CENTER_ALIGNMENT);
+        this.idTextField.setAlignmentX(JComponent.CENTER_ALIGNMENT);
+        this.refreshButton.setAlignmentX(JComponent.CENTER_ALIGNMENT);
     }
     
+    // lays out components
     {
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         this.add(Box.createVerticalStrut(10));
@@ -36,15 +65,18 @@ public class RequestsInfoPanel extends JPanel {
         this.add(Box.createVerticalStrut(10));
         this.add(this.label);
         this.add(Box.createVerticalStrut(10));
-        this.add(new JPanel() {
-            @Override
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                g.setColor(Color.green);
-                g.fillRect(0, 0, this.getWidth() - 1, this.getHeight() - 1);
-            }
-            
-        });
+        
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.add(this.idLabel);
+        panel.add(this.idTextField);
+        panel.add(Box.createVerticalStrut(50));
+        panel.add(this.refreshButton);
+        
+        JPanel horizontalPanel = new JPanel();
+        horizontalPanel.add(panel);
+        
+        this.add(horizontalPanel);
         this.add(Box.createVerticalStrut(10));
     }
 }
